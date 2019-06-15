80945580c82b76641861e161d4f04a06d8cb5c1a
Scott Idler <scott.a.idler@gmail.com>
2019-06-15 12:54:11 -0700

    saved some stuff

--- otto/load.py
+++ otto/load.py
@@ -9,61 +9,38 @@ from attrdict import AttrDict
 from otto.constants import *
 from otto.exceptions import OttoYmlLoadError
 
-OTTO_DEFAULTS = {
-    'jobs': OTTO_JOBS,
-    'version': OTTO_VERSION,
-}
 
-POS_DEFAULTS = dict(
-    action='store',
-    nargs=None,
-    const=None,
-    default=None,
-    choices=None,
-    metavar=None,
-)
+def get_dict(obj, key, default=None):
+    assert isinstance(obj, dict)
+    return obj.get(key, default or {})
 
-OPT_DEFAULTS = dict(POS_DEFAULTS,
-    required=False,
-    dest=None,
-)
+def get_list(obj, key, default=None):
+    assert isinstance(obj, dict)
+    return obj.get(key, default or [])
 
-def default(defaults, **yml):
-    return AttrDict(dict(defaults, **yml))
+def set_defaults(obj, defaults):
+    assert isinstance(obj, dict)
+    return dict(defaults, **obj)
 
-def get_dict(d, key, default=None):
-    return d.get(key, default or {})
+def default_args(obj):
+    assert isinstance(obj, dict)
+    obj['args'] = get_dict(obj, 'args')
+    for arg, body in obj['args'].items():
+        obj['args'][arg] = set_defaults(body, OPT_DEFAULTS if '-' in arg else POS_DEFAULTS)
+    return obj
 
-def get_list(d, key, default=None):
-    return d.get(key, default or [])
-
-def set_defaults(d, defaults):
-    return dict(defaults, **d)
-
-def default_otto(d):
-    d['otto'] = set_defaults(get_dict(d, 'otto'), OTTO_DEFAULTS)
-    return d
-
-def default_args(d):
-    d['args'] = get_dict(d, 'args')
-    for arg, body in d['args'].items():
-        d['args'][arg] = set_defaults(body, OPT_DEFAULTS if '-' in arg else POS_DEFAULTS)
-    return d
-
-def default_tasks(d):
-    d['tasks'] = get_dict(d, 'tasks')
-    for task, body in d['tasks'].items():
+def default_tasks(obj):
+    assert isinstance(obj, dict)
+    obj['tasks'] = get_dict(obj, 'tasks')
+    for task, body in obj['tasks'].items():
         body = default_args(body)
-        d['tasks'][task] = body
-    return d
+        obj['tasks'][task] = body
+    return obj
 
 def otto_load(otto_yml=OTTO_YML):
     try:
         otto_cfg = yaml.safe_load(open(otto_yml))
     except Exception as ex:
         raise OttoYmlLoadError(otto_yml) from ex
-
-    #otto_cfg = default_otto(otto_cfg)
-    #otto_cfg = default_args(otto_cfg)
     otto_cfg = default_tasks(otto_cfg)
     return AttrDict(otto_cfg)
---
+++ otto/task.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class OttoArgs():
+    def __init__(self,
+        name,
+        action=None,
+        nargs=None,
+        const=None,
+        default=None,
+        type=None,
+        choices=None,
+        required=None,
+        help=None
+        ):
+        pass
+
+class OttoTask():
+    def __init__(self, name, actions, deps=None, help=None, args=None, tasks=None):
+        self.name = name
+        self.args = args
+        self.tasks = tasks
+
