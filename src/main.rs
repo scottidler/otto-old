219469aeb0631bbc88a3cdd57d3ceabacf3401c9
Scott Idler <scott.a.idler@gmail.com>
2020-04-30 21:54:42 -0700

    implemented some serde for the otto.yml spec file

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,65 @@
+use std::fs::File;
+use std::io::Read;
+
+use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Point {
-    x: f64,
-    y: f64,
+fn default_otto() -> String {
+    "otto".to_string()
 }
 
-fn main() -> Result<(), serde_yaml::Error> {
-    let p1 = Point { x: 1.0, y: 2.0 };
-    println!("p1: {:?}", &p1);
+fn default_version() -> i32 {
+    1
+}
 
-    let s = serde_yaml::to_string(&p1)?;
-    assert_eq!(s, "---\nx: 1.0\ny: 2.0");
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Spec {
+    otto: Otto,
+}
 
-    let p2: Point = serde_yaml::from_str(&s)?;
-    assert_eq!(p1, p2);
-    println!("p2: {:?}", &p2);
-    Ok(())
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Otto {
+    #[serde(default = "default_version")]
+    version: i32,
+
+    #[serde(default = "default_otto")]
+    name: String,
+
+    params: Option<HashMap<String, Param>>,
+
+    action: Option<String>,
+
+    tasks: Option<HashMap<String, Task>>,
+}
+
+#[derive(Default, Debug, PartialEq, Serialize, Deserialize)]
+struct Task {
+    name: Option<String>,
+
+    params: Option<HashMap<String, Param>>,
+
+    action: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Param {
+    name: Option<String>,
+
+    help: Option<String>,
+}
+
+fn main() {
+    let filename = "examples/ex1.yml";
+    match File::open(filename) {
+        Ok(mut file) => {
+            let mut content = String::new();
+            file.read_to_string(&mut content).unwrap();
+
+            let spec: Spec = serde_yaml::from_str(&content).unwrap();
+            println!("{:?}", spec);
+        }
+        Err(error) => {
+            println!("There is an error {}: {}", filename, error);
+        }
+    }
 }
